c5e66388033303055cdaea490f339d618d33a89a
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-18 13:20:26 -0700

    trouble-shooting using solution code, updated handleSubmitWord() and player_guess() to print input variables

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
-boggle_game = Boggle()
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 app.debug = True
@@ -10,18 +10,23 @@ app.debug = True
 toolbar = DebugToolbarExtension(app)
 # app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
 
-@app.route('/')
+boggle_game = Boggle()
+
+@app.route('/', methods=["POST"])
 def index():
     '''homepage, render game board'''
     board = boggle_game.make_board()
     session['board'] = board
     return render_template("index.html", board=board)
 
-@app.route('/player_guess')
+@app.route('/player_guess', methods=["POST"])
 def player_guess():
-    '''collects player guess from JS class, checks if guess is valid'''
-    guess = request.args["word_guess"]
-    raise
+    '''collects player guess from class BoggleGame'''
+    # word = get(req)
+    # print(word)
+    # # print(request.__dict__)
+    return "200"
+    # guess = request.args["word_guess"]
     # print(guess)
     # board = session["board"]
     # print(board)
--- static/script.js
+++ static/script.js
@@ -1,7 +1,22 @@
 class BoggleGame {
-    constructor() {
-        $(.word_guess).on("submit", this.handleSubmit.bind(this));
+    // new game instance
+    constructor(boardId) {
+        // # create ID for game instance
+        this.board = $("#" + boardId);
+
+        $(".add-word", this.board).on("submit", this.handleSubmitWord.bind(this));
     }
-    // send word_guess to server
-    // check for validity
+    // event handler for submitted word, collects word value
+     async handleSubmitWord(e) {
+        e.preventDefault();
+
+         const $word = $(".word", this.board);
+
+         let word = $word.val();
+         console.log(word)
+         // if (!word) return;
+
+        // let $guessedWord = await axios.get(".player_guess", this.board)
+        // console.log($guessedWord)
+    }
 }
