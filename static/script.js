5f62e4f2a8870accf0be372c5d179bac746698db
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-18 14:23:34 -0700

    implemented new Set() to BoggleGame, validate guess and added to set

--- static/script.js
+++ static/script.js
@@ -3,7 +3,7 @@ class BoggleGame {
     constructor(boardId) {
         // # create ID for game instance
         this.board = $("#" + boardId);
-
+        this.guesses = new Set()
         $(".player-guess", this.board).on("submit", this.handleSubmitWord.bind(this));
     }
     // event handler for submitted word, collects word value
@@ -13,7 +13,14 @@ class BoggleGame {
          const $word_guess = $(".word_guess", this.board);
 
          let word_guess = $word_guess.val();
-         console.log(word_guess)
-
+         // ckeck if word was submitted
+         if (!word_guess) return;
+         // check if word has already been submitted and is duplicate
+         if (this.guesses.has(word_guess)) {
+             console.log('error - this.guesses.has(word_guess)')
+         }
+         // add guess to set
+         this.guesses.add(word_guess)
+         console.log(this.guesses)
     }
 }
