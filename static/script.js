ed6901b89c82a9b1c7779f7baabda11dc7a6dfaf
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-19 14:23:07 -0700

    updated files to add timer - not working

--- static/script.js
+++ static/script.js
@@ -5,7 +5,7 @@ class BoggleGame {
         this.board = $("#" + boardId);
         this.guesses = new Set()
         this.score = 0;
-        this.timer = 60
+        // this.gameTimer()
         // this.progressTimer = $(".timer")
         // console.log(progressTimer)
 
@@ -13,61 +13,79 @@ class BoggleGame {
         // $("#start").addEventListener('click', ev => this.OnEvent(e), this.board).onclick(function(e) {})
         $(".player-guess", this.board).on("submit", this.handleSubmitWord.bind(this));
     }
+
     // event handler for submitted word, collects word value
-     async handleSubmitWord(e) {
+    async handleSubmitWord(e) {
         e.preventDefault();
 
-         const $word_guess = $(".word_guess", this.board);
+        const $word_guess = $(".word_guess", this.board);
 
-         let word_guess = $word_guess.val();
-         // check if word was submitted
-         if (!word_guess) return;
-         // check if word has already been submitted
-         if (this.guesses.has(word_guess)) {
-             console.log('error')
-             this.messageToPlayer("Duplicate word","error");
-             return;
-         }
-         // add guess to set - *** code is different from solution ***
-         this.guesses.add(word_guess)
-            console.log(this.guesses)
+        let word_guess = $word_guess.val();
+        // check if word was submitted
+        if (!word_guess) return;
+        // check if word has already been submitted
+        if (this.guesses.has(word_guess)) {
+            console.log('error')
+            this.messageToPlayer("Duplicate word", "error");
+            return;
+        }
+        // add guess to set - *** code is different from solution ***
+        this.guesses.add(word_guess)
+        console.log(this.guesses)
 
 
-         //check if guess is a valid word on the server
-         const resp = await axios.get("/player_guess", { params: { word_guess: word_guess}});
-         if (resp.data.result === "not-word") {
-             console.log('not valid word')
-             this.messageToPlayer(`${word_guess} is not a valid word`, "error")
-         } else if (resp.data.result === "not-on-board") {
-             console.log("not a valid game word")
-             this.messageToPlayer(`${word_guess} is not included on board`)
-         } else {
-             console.log('match')
-             this.messageToPlayer(`Great job!`, 'match')
-             // # add length of word to player score
-             this.score += word_guess.length
-             console.log(this.score)
-             this.playerScore()
-         }
+        //check if guess is a valid word on the server
+        const resp = await axios.get("/player_guess", {params: {word_guess: word_guess}});
+        if (resp.data.result === "not-word") {
+            console.log('not valid word')
+            this.messageToPlayer(`${word_guess} is not a valid word`, "error")
+        } else if (resp.data.result === "not-on-board") {
+            console.log("not a valid game word")
+            this.messageToPlayer(`${word_guess} is not included on board`)
+        } else {
+            console.log('match')
+            this.messageToPlayer(`Great job!`, 'match')
+            // # add length of word to player score
+            this.score += word_guess.length
+            console.log(this.score)
+            this.playerScore()
+        }
     }
+
     // show message to player
     messageToPlayer(message, messageType) {
         $(".message", this.board).text(message).addClass(`message, ${messageType}`)
     }
+
     // show and update player score
     playerScore() {
         $(".score", this.board).text(this.score);
     }
+
     // gameboard timer - 60 second progress bar
 
-    gameTimer(e) {
-        e.preventDefault();
-        // console.log('here');
-        let gameTime = $('.timer').progressBarTimer()
-        gameTime.start()
+    // gameTimer() {
+    //     // e.preventDefault();
+    //     // console.log('here');
+    //     // let gameTime = $('.timer').progressBarTimer()
+    //     // gameTime.start()
+    //
+    //     $('.progress').progressBarTimer({autostart: true})
+    //
+    //     // show final score on finish
+    //     // onFinish:function() {}
+    // }
 
-        // show final score on finish
-        // onFinish:function() {}
-
-    }
+    // gameTimer() {
+    //     $("#progressTimer").progressTimer({
+    //         timeLimit: 120,
+    //         warningThreshold: 10,
+    //         baseStyle: 'progress-bar-warning',
+    //         warningStyle: 'progress-bar-danger',
+    //         completeStyle: 'progress-bar-info',
+    //         onFinish: function () {
+    //             console.log("I'm done");
+    //         }
+    //     });
+    // }
 }
