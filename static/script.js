0162ebf1f90bbd6078e49d6b8e8762b0005af9c4
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-18 17:56:42 -0700

    added messages to player and created element for message

--- static/script.js
+++ static/script.js
@@ -13,25 +13,34 @@ class BoggleGame {
          const $word_guess = $(".word_guess", this.board);
 
          let word_guess = $word_guess.val();
-         // ckeck if word was submitted
+         // check if word was submitted
          if (!word_guess) return;
          // check if word has already been submitted
          if (this.guesses.has(word_guess)) {
-             console.log('error - this.guesses.has(word_guess)')
+             console.log('error')
+             this.messageToPlayer("Duplicate word","error");
+             return;
          }
          // add guess to set - *** code is different from solution ***
          this.guesses.add(word_guess)
-         console.log(this.guesses)
+            console.log(this.guesses)
+
 
          //check if guess is a valid word on the server
          const resp = await axios.get("/player_guess", { params: { word_guess: word_guess}});
          if (resp.data.result === "not-word") {
              console.log('not valid word')
+             this.messageToPlayer(`${word_guess} is not a valid word`, "error")
          } else if (resp.data.result === "not-on-board") {
              console.log("not a valid game word")
+             this.messageToPlayer(`${word_guess} is not included on board`)
          } else {
              console.log('match')
+             this.messageToPlayer(`Great job!`, 'match')
          }
-
+    }
+    // show message to player
+    messageToPlayer(message, messageType) {
+        $(".message", this.board).text(message).addClass(`message, ${messageType}`)
     }
 }
